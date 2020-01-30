cee97a64a9859f26a9b86350afe2428ecd2cd6a3
LeslieWilson <leslie.wilson.vt@gmail.com>
2020-01-30 18:22:29 -0500

    variable placeholder for authorization token made

--- app/controllers/api/v1/patient_info_controller.rb
+++ app/controllers/api/v1/patient_info_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::PatientInfoController < ApplicationController
 
   def index
-
-    response = HTTP.auth("Bearer 7359d7ca6855487fa05eec214099d4f9").get('https://api.1up.health/fhir/dstu2/Patient/f8fedcd9e6e5/$everything')
+    authorizationToken ='authorization token goes here'
+    response = HTTP.auth("Bearer #{authorizationToken}").get('https://api.1up.health/fhir/dstu2/Patient/f8fedcd9e6e5/$everything')
     render json: JSON.parse(response.to_s)
   end
 end
